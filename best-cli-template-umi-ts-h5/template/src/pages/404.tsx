35a96729be2a40256de7cb306e4fd7307bc96966
罗智毅 <zhiyiluo@drgou.com>
2021-08-03 17:40:00 +0800

    404页面调整

--- best-cli-template-umi-ts-h5/template/src/pages/404.tsx
+++ best-cli-template-umi-ts-h5/template/src/pages/404.tsx
@@ -1,10 +1,3 @@
-import { Card } from 'antd';
 export default function Page404() {
-  return (
-    <Card bordered={false}>
-      <h3 style={{ textAlign: 'center', fontSize: '32px', margin: '60px 0' }}>
-        404
-      </h3>
-    </Card>
-  );
+  return <h3 style={{ textAlign: 'center', fontSize: '32px', margin: '60px 0' }}>404</h3>;
 }
