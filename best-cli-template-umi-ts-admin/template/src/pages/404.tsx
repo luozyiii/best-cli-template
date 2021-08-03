f9f6b7514f9d1a2c01bb5e4aeb885b89adf9e3ac
罗智毅 <zhiyiluo@drgou.com>
2021-08-03 17:41:37 +0800

    优化

--- best-cli-template-umi-ts-admin/template/src/pages/404.tsx
+++ best-cli-template-umi-ts-admin/template/src/pages/404.tsx
@@ -1,8 +1,3 @@
-import { Card } from 'antd';
-export default function IndexPage() {
-  return (
-    <Card bordered={false}>
-      <h3 style={{ textAlign: 'center', fontSize: '32px', margin: '60px 0' }}>404</h3>
-    </Card>
-  );
+export default function Page404() {
+  return <h3 style={{ textAlign: 'center', fontSize: '32px', margin: '60px 0' }}>404</h3>;
 }
